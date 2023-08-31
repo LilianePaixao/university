60a27bc13487cb90590abaae09b77840112e81ec
Liliane <cpaixaoliliane@gmail.com>
2023-08-31 08:03:42 -0300

    Plugin:Multiple Choice Block Type(React)

---
+++ wordpress/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => '3dcf92beb3e460e310ca');
---
+++ wordpress/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element'), 'version' => 'aea3ef2f25496cce5974');
